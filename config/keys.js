776c1f104d6adb0bca5d0131a13993083d78055b
David Martin <dmartin5150@icloud.com>
2023-10-27 20:37:51 -0500

    added env vars

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// figure out what set of keys to return
+
+if (process.env.NODE_ENV === 'production') {
+     module.exports = require('/prod');
+} else {
+     module.exports = require('./dev');
+}
+
