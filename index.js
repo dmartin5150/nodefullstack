3c63e6569042b94a7bd784e16ca5c95d1595ba3b
David Martin <dmartin5150@icloud.com>
2023-10-27 19:14:48 -0500

    finished initial authentication

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+require('./models/Users');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./services/passport');
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-     res.send({hi:'there'});
-});
+app.use(cookieSession({
+     maxAge: 30 *24 * 60 * 60 * 1000,
+     keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
