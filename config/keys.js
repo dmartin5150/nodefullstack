2304f550aef87475933fb0650d30e79e968c0ca2
David Martin <dmartin5150@icloud.com>
2023-10-27 20:40:37 -0500

    fixed typo in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // figure out what set of keys to return
 
 if (process.env.NODE_ENV === 'production') {
-     module.exports = require('/prod');
+     module.exports = require('./prod');
 } else {
      module.exports = require('./dev');
 }
